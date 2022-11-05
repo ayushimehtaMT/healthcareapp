2bc222f1bbc50dabe76eea311665ffd5cde0417c
kanakalakshmi <2021mt93527@wilp.bits-pilani.ac.in>
2022-11-06 00:06:43 +0530

    search login checkout getorderedList api

--- functions/index.js
+++ functions/index.js
@@ -18,19 +18,14 @@ app.use(cors({ origin: true }))
 //Main Database refference
 const db = admin.firestore();
 
-//Create get
-app.get('/',(req,res) => {
-return res.status(200).send('Test API response')
-})
-
 //Create post
-app.post("/api/login/",(req,res) => {
+app.post("/api/v1/login/",(req,res) => {
     (async () => {
         try {
             await db.collection("userDetails").doc(`/${Date.now()}/`).create({
                 id: Date.now(),
                 name: req.body.name,
-                mobile: req.body.password,
+                password: req.body.password,
             })
             return res.status(200).send({status: "Success", msg: "data saved"})
         }
@@ -40,6 +35,32 @@ app.post("/api/login/",(req,res) => {
         }
     })()
 })
+
+app.get('/api/v1/search/:id', async (req, resp) => {
+    db.collection('medicineList').where("id", "==", req.params.id)
+        .get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                resp.status(200).send(doc.data());
+        })
+    });
+});
+
+app.get('/api/v1/checkout/:userId', async (req, resp) => {
+  db.collection('orderedList').where("userId", "==", req.params.userId)
+      .get()
+      .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+              resp.status(200).send(doc.data());
+      })
+  });
+});
+
+app.post("/api/v1/checkout", async (req, resp) => {
+  const cart = req.body;
+  await db.collection('orderedList').add(cart);
+  resp.status(201).send();
+});
 
 //export our api to firebase and cloud functions
 exports.app = functions.https.onRequest(app)
